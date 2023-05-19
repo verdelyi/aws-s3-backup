04605572e7f42ab55251c44188e4a34bcd016356
Viktor Erdelyi <verdelyi@users.noreply.github.com>
2023-05-19 15:27:48 +0900

    minor tweak

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ echo "Building AWS S3 backup app..."
 ./gradlew distZip
 
 echo "clean & recreate remote directory"
-ssh $TARGETHOST "rm -rf aws-s3-backup && mkdir -p aws-s3-backup"
+ssh "$TARGETHOST" "rm -rf aws-s3-backup && mkdir -p aws-s3-backup"
 
 echo "copy distribution zip"
 scp build/distributions/aws-s3-backup.zip "$TARGETHOST:aws-s3-backup/"
 
 echo "unpack"
-ssh $TARGETHOST "unzip -q aws-s3-backup/aws-s3-backup.zip && rm aws-s3-backup/aws-s3-backup.zip"
+ssh "$TARGETHOST" "unzip -q aws-s3-backup/aws-s3-backup.zip && rm aws-s3-backup/aws-s3-backup.zip"
