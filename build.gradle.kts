4eaef33fa74eb3e7ef40539a7e8fb7070b2df751
Viktor Erdelyi <verdelyi@example.com>
2024-11-30 20:22:13 +0900

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.1.0"
     application
 }
 
@@ -12,15 +12,14 @@ application {
 }
 
 dependencies {
-    implementation(platform("software.amazon.awssdk:bom:2.26.21"))
+    implementation(platform("software.amazon.awssdk:bom:2.29.23"))
     implementation("software.amazon.awssdk:s3")
     implementation("software.amazon.awssdk:s3-transfer-manager")
-    implementation("software.amazon.awssdk.crt:aws-crt:0.30.0") // not managed by the bom apparently
+    implementation("software.amazon.awssdk.crt:aws-crt:0.33.3") // not managed by the bom apparently
     implementation("com.amazonaws:aws-encryption-sdk-java:3.0.1") // not managed by the bom apparently
     implementation("org.slf4j:slf4j-simple:2.0.7")
     //implementation("javax.xml.bind:jaxb-api:2.3.1") // for AWS SDK on Java 9+
-    implementation("org.bouncycastle:bcprov-ext-jdk18on:1.78")
-
+    implementation("org.bouncycastle:bcprov-ext-jdk18on:1.78.1")
 }
 
 kotlin {
