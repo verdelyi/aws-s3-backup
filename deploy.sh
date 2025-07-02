79033e03269ad44f5d5f2bcbce5796c042581503
Viktor Erdelyi <verdelyi@example.com>
2025-07-02 13:23:02 +0900

    script update

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 set -e
-
-# Target host to deploy to. Reasonable options are "nsf-jst-cloudserver" and "nsf-jst-localserver"
 TARGETHOST="$1"
-
 echo "Building AWS S3 backup app..."
 ./gradlew distZip
-
-echo "clean & recreate remote directory"
-ssh "$TARGETHOST" "rm -rf aws-s3-backup && mkdir -p aws-s3-backup"
-
-echo "copy distribution zip"
+echo "Deploying to $TARGETHOST..."
+ssh "$TARGETHOST" "rm -rf aws-s3-backup && mkdir aws-s3-backup"
 scp build/distributions/aws-s3-backup.zip "$TARGETHOST:aws-s3-backup/"
-
-echo "unpack"
-ssh "$TARGETHOST" "unzip -q aws-s3-backup/aws-s3-backup.zip && rm aws-s3-backup/aws-s3-backup.zip"
+ssh "$TARGETHOST" "cd aws-s3-backup && unzip -q aws-s3-backup.zip && rm aws-s3-backup.zip"
