fe847dd3ac626df5a9bb5e85d52153491dcf4063
Viktor Erdelyi <verdelyi@example.com>
2026-05-02 00:11:18 +0900

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "2.3.21"
     application
 }
 
@@ -12,21 +12,21 @@ application {
 }
 
 dependencies {
-    implementation(platform("software.amazon.awssdk:bom:2.35.0"))
+    implementation(platform("software.amazon.awssdk:bom:2.43.1"))
     implementation("software.amazon.awssdk:s3")
     implementation("software.amazon.awssdk:s3-transfer-manager")
-    implementation("software.amazon.awssdk.crt:aws-crt:0.39.0") // not managed by the bom apparently
+    implementation("software.amazon.awssdk.crt:aws-crt:0.45.2") // not managed by the bom apparently
     implementation("com.amazonaws:aws-encryption-sdk-java:3.0.2") // not managed by the bom apparently
     implementation("org.slf4j:slf4j-simple:2.0.17")
     //implementation("javax.xml.bind:jaxb-api:2.3.1") // for AWS SDK on Java 9+
     //implementation("org.bouncycastle:bcprov-jdk18on:1.81")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:6.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 tasks.test {
